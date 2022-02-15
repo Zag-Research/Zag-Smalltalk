278e734fb1c47b63a89ceff3d4ca755b04489fa6
Dave Mason <dmason@ryerson.ca>
2022-02-14 23:29:16 -0500

    short_class
    Bring decoding in line with Documentation

--- c/ast.h
+++ c/ast.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //#define print(t,v) printf("%-10s 0x%lx\n",t,v)
-#define print(t,v) printf("%-10s 0x%lx %d\n",t,v,get_class(v))
+#define print(t,v) printf("%-10s 0x%lx %d\n",t,v,short_class(v))
 typedef unsigned long objectT;
 #define negative_inf 0xfff0000000000000
 #define false 0xfff4000000000000
@@ -8,7 +8,7 @@ typedef unsigned long objectT;
 #define nil 0xfff8000000000002
 #define symbol_of(index,arity) ((index)|((arity)<<20)|(0x7ffdl<<49))
 #define character_of(c) ((objectT)(c))|(0x7ffel<<49)
-#define from_int(x) (((objectT)(x))|(0x7fffl<<49))
+#define from_int(x) (((objectT)(x))+int_zero)
 #define int_zero 0xfffe000000000000
 static inline objectT from_double(const double x) {
   union {objectT ul;double d;} y;
@@ -21,7 +21,8 @@ static inline double as_double(const objectT x) {
   y.ul=x;
   return y.d;
 }
-#define get_class(x) (((objectT)(x)>>49)>0x7ff0?((int)((objectT)(x)>>49)&7):((objectT)(x)>>49)<0x7ff0?8:get_class_7ff0(x))
+#define get_class(x) (((objectT)(x)>>49)>0x7ff8?((int)((objectT)(x)>>49)&7):((objectT)(x)>>49)<0x7ff0?8:get_class_7ff0(x))
+#define short_class(x) (((objectT)(x))>0xfff0000000000000?((int)((objectT)(x)>>49)&7):8)
 static inline int get_class_7ff0(const objectT x) {
   long ptr = (((long)(x))<<15>>12);
   if (ptr==0) return 8;
--- c/eg1.c
+++ c/eg1.c
@@ -1,7 +1,9 @@
 #include "ast.h"
+#include <math.h>
 #define S_foo symbol_of(42,0)
 #define S_bar_ symbol_of(43,1)
 int main(int argc,char **argv) {
+  print("object",from_double(-INFINITY)+1);
   print("false",false);
   print("true",true);
   print("nil",nil);
@@ -10,5 +12,7 @@ int main(int argc,char **argv) {
   print("$c",character_of('c'));
   print("42",from_int(42));
   print("42.0",from_double(42.0));
+  print("inf",from_double(INFINITY));
+  print("-inf",from_double(-INFINITY));
   return 0;
 }
