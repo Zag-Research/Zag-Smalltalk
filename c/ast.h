a1bd0fd03fb5968da94170d2efe604ed741c8676
Dave Mason <dmason@ryerson.ca>
2022-02-16 14:28:47 -0500

    Exploiting CPP macros

--- c/ast.h
+++ c/ast.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //#define print(t,v) printf("%-10s 0x%lx\n",t,v)
-#define print(t,v) printf("%-10s 0x%lx %d %s\n",t,v,short_class(v),printString(v))
+#define print(v) ({typeof (v) V = (v);printf("%-25s 0x%lx %d %s\n",#v,V,short_class(V),printString(V));})
 typedef unsigned long objectT;
 #define negative_inf 0xfff0000000000000
 #define false 0xfff4000000010000
@@ -9,20 +9,20 @@ typedef unsigned long objectT;
 #define symbol_of(index,arity) ((index)|((arity)<<20)|(0x7ffdl<<49))
 #define from_object(addr) ((((long)(void*)addr)>>3)|(0x7ff8l<<49))
 #define from_closure(addr) ((((long)(void*)addr)>>3)|(0x7ff9l<<49))
-#define character_of(c) ((objectT)(c))|(0x7ffel<<49)
+#define from_char(c) ((objectT)(c))|(0x7ffel<<49)
 #define from_int(x) (((objectT)(x))+int_zero)
 #define int_zero 0xffff000000000000
-static inline objectT from_double(const double x) {
-  union {objectT ul;double d;} y;
-  y.d=x;
-  return y.ul;
-}
+#define from_double(x) ({         \
+  union {objectT ul;double d;} y_; \
+  y_.d=x;                          \
+  y_.ul;                           \
+    })
 #define as_int(x) (((long)(x))-int_zero)
-static inline double as_double(const objectT x) {
-  union {objectT ul;double d;} y;
-  y.ul=x;
-  return y.d;
-}
+#define as_double(x) ({            \
+  union {objectT ul;double d;} y_; \
+  y_.ul=x;                         \
+  y_.d;                            \
+})
 #define as_pointer(x) (((long)(x))<<15>>12)
 #define get_class(x) (((objectT)(x)>>49)>0x7ff8?((int)((objectT)(x)>>49)&7):((objectT)(x)>>49)<0x7ff0?8:get_class_7ff0(x))
 #define short_class(x) (((objectT)(x))>0xfff0000000000000?((int)((objectT)(x)>>49)&7):8)
--- c/eg1.c
+++ c/eg1.c
@@ -4,20 +4,20 @@ char *symbol_table[]={0,"sym1","foo","bar:","sym4"};
 #define S_foo symbol_of(2,0)
 #define S_bar_ symbol_of(3,1)
 int main(int argc,char **argv) {
-  print("object",from_double(-INFINITY)+1);
-  print("object",from_object(&main));
-  print("object",from_object(&argc));
-  print("closure",from_closure(&main));
-  print("false",false);
-  print("true",true);
-  print("nil",nil);
-  print("S_foo",S_foo);
-  print("S_bar_",S_bar_);
-  print("$c",character_of('c'));
-  print("-42",from_int(-42));
-  print("42",from_int(42));
-  print("42.7",from_double(42.7));
-  print("inf",from_double(INFINITY));
-  print("-inf",from_double(-INFINITY));
+  print(from_double(-INFINITY)+1);
+  print(from_object(&main));
+  print(from_object(&argc));
+  print(from_closure(&main));
+  print(false);
+  print(true);
+  print(nil);
+  print(S_foo);
+  print(S_bar_);
+  print(from_char('c'));
+  print(from_int(-42));
+  print(from_int(42));
+  print(from_double(42.7));
+  print(from_double(INFINITY));
+  print(from_double(-INFINITY));
   return 0;
 }
