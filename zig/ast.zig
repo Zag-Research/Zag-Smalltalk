1ab2b3a0721bd3fddf0363bfab99e9e3dfa70cce
Dave Mason <dmason@ryerson.ca>
2022-02-23 08:02:38 -0500

    made more generic

--- zig/ast.zig
+++ zig/ast.zig
@@ -121,13 +121,12 @@ const methodT = fn(stack : [*]Object, heap : [*]Object) returnE;
 test "thread 0 initialization" {
     thread = threadT.init();
 }
-const n_primes : usize = 20;
-fn gen_primes() [n_primes]u32 {
-    var p : [n_primes]u32 = undefined;
-    var possible : u32 = 7;
-    var previous : u32 = 0;
+fn gen_primes(comptime T : type, n_primes: usize) [n_primes]T {
+    var p : [n_primes]T = undefined;
+    var possible : T = 7;
+    var previous : T = 0;
     var i: usize = 0;
-    @setEvalBranchQuota(100000);
+    if (n_primes>13) @setEvalBranchQuota(100000);
     next:
         while (true) : (possible += 2) {
             var j: usize = 3;
@@ -141,10 +140,10 @@ fn gen_primes() [n_primes]u32 {
             if (i>=n_primes) return p;
     }
 }
-const prime_values : [n_primes]u32 = gen_primes();
+const prime_values = gen_primes(u32,13);
 pub fn next_prime_larger_than(n : u32) u32 {
     var low : usize = 0;
-    var high : usize = n_primes-1;
+    var high : usize = prime_values.len-1;
     while (low<=high) {
         const mid = (low+high)/2;
         if (mid==0) return prime_values[0];
@@ -157,14 +156,18 @@ pub fn next_prime_larger_than(n : u32) u32 {
     return 11959;
 }
 test "primes" {
-    const expect = @import("std").testing.expect;
-    try expect(next_prime_larger_than(3)==7);
-    try expect(next_prime_larger_than(25)==29);
-    try expect(next_prime_larger_than(167)==167);
-    try expect(next_prime_larger_than(1889)==1889);
-    try expect(next_prime_larger_than(1890)==11959);
 //    const stdout = @import("std").io.getStdOut().writer();
 //    try stdout.print("primes: {any}\n",.{prime_values});
+    const expect = @import("std").testing.expect;
+    try expect(next_prime_larger_than(3)==7);
+    try expect(next_prime_larger_than(24)==29);
+    try expect(next_prime_larger_than(167)==167);
+    try expect(next_prime_larger_than(224)==293);
+    if (prime_values.len<20) {
+        try expect(next_prime_larger_than(294)==11959);
+    } else
+        try expect(next_prime_larger_than(1889)==1889);
+    try expect(next_prime_larger_than(1890)==11959);
 }
 
 //#include <stdio.h>
