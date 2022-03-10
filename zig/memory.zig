a1e50c6e4d9a6decec442966287a598d3af17653
Dave Mason <dmason@ryerson.ca>
2022-03-10 08:09:50 -0500

    Combined heap and memory

--- zig/memory.zig
+++ zig/memory.zig
@@ -1,9 +1,155 @@
+const page_size = @import("std").mem.page_size/@sizeOf(Header);
 const object = @import("object.zig");
 const Object = object.Object;
 const mem = @import("std").mem;
 const Allocator = mem.Allocator;
 const class = @import("class.zig");
-const heap = @import("heap.zig");
+const native_endian = @import("builtin").target.cpu.arch.endian();
+pub const Format = enum(u8) {
+    none = 0,
+    object = InstVars,
+    array = Indexable,
+    both = InstVars + Indexable,
+    weak = InstVars + Indexable + Weak,
+    objectNP = InstVars + PointerFree,
+    arrayNP = Indexable + PointerFree,
+    bothNP = InstVars + Indexable + PointerFree,
+    raw64 = Indexable_64,
+    _,
+    const Self = @This();
+    const InstVars : u8 = 1;
+    const Indexable : u8 = 2;
+    const Weak : u8 = 4;
+    const PointerFree : u8 = 8;
+    const RawData : u8 = 16;
+    const Indexable_64 : u8 = 17;
+    const Indexable_32 : u8 = 18;
+    const Indexable_16 : u8 = 20;
+    const Indexable_8 : u8 = 24;
+    const BaseFormat : u8 = 31;
+    const Immutable : u8 = 32;
+    pub inline fn weak(self: Self) Self {
+        return @intToEnum(Self,(@bitCast(u8,self) & ~BaseFormat) + InstVars + Indexable + Weak);
+    }
+    pub inline fn base(self: Self) Self {
+        return @intToEnum(Self,(@bitCast(u8,self) & ~BaseFormat));
+    }
+    pub inline fn object(self: Self) Self {
+        return @intToEnum(Self,@bitCast(u8,self) + InstVars);
+    }
+    pub inline fn array(self: Self) Self {
+        return @intToEnum(Self,@bitCast(u8,self) + Indexable);
+    }
+    pub inline fn raw(self: Self, T : type, size : usize) Self {
+        switch (T) {
+            u8,i8 => {return @intToEnum(Self,(@bitCast(u8,self) & ~BaseFormat) + Indexable_8 + ((-@intCast(isize,size))&7));},
+            u16,i16 => {return @intToEnum(Self,(@bitCast(u16,self) & ~BaseFormat) + Indexable_16 + ((-@intCast(isize,size))&3));},
+            u32,i32,f32 => {return @intToEnum(Self,(@bitCast(u32,self) & ~BaseFormat) + Indexable_32 + ((-@intCast(isize,size))&1));},
+            u64,i64,f64 => {return @intToEnum(Self,(@bitCast(u64,self) & ~BaseFormat) + Indexable_64);},
+            else => {return self;},
+        }
+    }
+    pub inline fn hasInstVars(self: Self) bool {
+        return @bitCast(u8, self) & InstVars != 0 and @bitCast(u8, self) & BaseFormat < RawData;
+    }
+    pub inline fn isIndexable(self: Self) bool {
+        return @bitCast(u8, self) & Indexable != 0 or @bitCast(u8, self) & BaseFormat >= RawData;
+    }
+    pub inline fn hasBoth(self: Self) bool {
+        return @bitCast(u8, self) & InstVars+Indexable == InstVars+Indexable and @bitCast(u8, self) & BaseFormat < RawData;
+    }
+    pub inline fn isWeak(self: Self) bool {
+        return @bitCast(u8, self) & Weak != 0 and @bitCast(u8, self) & BaseFormat < RawData;
+    }
+    pub inline fn isPointerFree(self: Self) bool {
+        return @bitCast(u8, self) & BaseFormat >= PointerFree;
+    }
+    pub inline fn hasPointers(self: Self) bool {
+        return ~self.isPointerFree();
+    }
+    pub inline fn isImmutable(self: Self) bool {
+        return @bitCast(u8, self) & Immutable != 0;
+    }
+    pub inline fn is_64(self: Self) bool {
+        return @bitCast(u8, self) & RawData != 0 and @bitCast(u8, self) & BaseFormat == Indexable_64;
+    }
+    pub inline fn is_32(self: Self) bool {
+        return @bitCast(u8, self) & RawData != 0 and @bitCast(u8, self) & BaseFormat >= Indexable_32 and @bitCast(u8, self) & BaseFormat <= Indexable_32 + 1;
+    }
+    pub inline fn is_16(self: Self) bool {
+        return @bitCast(u8, self) & RawData != 0 and @bitCast(u8, self) & BaseFormat >= Indexable_16 and @bitCast(u8, self) & BaseFormat <= Indexable_16 + 3;
+    }
+    pub inline fn is_8(self: Self) bool {
+        return @bitCast(u8, self) & RawData != 0 and @bitCast(u8, self) & BaseFormat >= Indexable_8 and @bitCast(u8, self) & BaseFormat <= Indexable_8 + 7;
+    }
+};
+pub const HeapPtr = *Header;
+const heapMethods = struct {
+    pub inline fn asObject(self: Header) Object {
+        return @bitCast(Object,self);
+    }
+    pub inline fn setHash(self: *Header,hash: u24) Header {
+        self.hash=hash;
+        return self.*;
+    }
+    pub inline fn setNArgs(self: *Header,args: u8) Header {
+        self.hash=(self.hash & 0xffff)+(args<<16);
+        return self.*;
+    }
+    pub inline fn instVars(self: HeapPtr) []Object {
+        if (self.format.hasInstVars()) {
+            const size = self.length;
+            return self.asObjectArray()[1..size+1];
+        } else return &[0]Object{};
+    }
+    pub inline fn indexables(self: HeapPtr,comptime T:type) ![]T {
+        if (self.format.isIndexable()) {
+            const size = self.length;
+            return self.asObjectArray()[1..size+1];
+        } else return error.NotIndexable;
+    }
+    pub inline fn asObjectArray(self: HeapPtr) [*]Object {
+        return @ptrCast([*]Object,self);
+    }
+    pub inline fn derefForwarded(self: HeapPtr) HeapPtr {
+        return @intToPtr(HeapPtr,-@bitCast(i64,self.*));
+    }
+    pub inline fn totalSize(self: HeapPtr) usize {
+        if (self.format.hasBoth()) unreachable;
+        var size = self.length;
+        return size+1;
+    }
+};
+const Header = switch (native_endian) {
+    .Big => packed struct {
+        length: u16,
+        format: Format,
+        hash: u24,
+        classIndex: u16,
+        usingnamespace heapMethods;
+    },
+    .Little => packed struct {
+        classIndex: u16,
+        hash: u24,
+        format: Format,
+        length: u16,
+        usingnamespace heapMethods;
+    },
+};
+pub inline fn header(length : u16, format : Format, classIndex : u16, hash: u24) Header {
+    return Header {
+        .length = length,
+        .format = format,
+        .hash = hash,
+        .classIndex = classIndex,
+    };
+}
+test "Header structure" {
+    const expect = @import("std").testing.expect;
+    try expect(@sizeOf(Header) == 8);
+    const hdr = header(17, Format.object, 35,0x123);
+    try expect(@bitCast(u64, hdr) == 0x0011010001230023);
+}
 const ArenaType = enum {
     Nursery,
     Teen,
@@ -11,7 +157,7 @@ const ArenaType = enum {
     Test,
 };
 const Arena = struct {
-    heap: heap.HeapPtr,
+    heap: HeapPtr,
     tos: [*]Object,
     allocated: []Object,
     allocator: Allocator,
@@ -21,7 +167,7 @@ const Arena = struct {
         const allocated = allocator.alloc(Object,size) catch |err| return err;
         return Self {
             .tos = allocated.ptr+allocated.len,
-            .heap = @ptrCast(heap.HeapPtr,allocated.ptr),
+            .heap = @ptrCast(HeapPtr,allocated.ptr),
             .allocated = allocated,
             .allocator = allocator,
             .arenaType = arenaType,
@@ -31,7 +177,7 @@ const Arena = struct {
         self.allocator.free(self.allocated);
         self.* = undefined;
     }
-    pub fn alloc(self : *Self, classIndex : class.ClassIndex, format: heap.Format, iv_size : usize, array_size : usize, fill: anytype) !heap.HeapPtr {
+    pub fn alloc(self : *Self, classIndex : class.ClassIndex, format: Format, iv_size : usize, array_size : usize, fill: anytype) !HeapPtr {
         var form = format.base();
         var size = iv_size + array_size;
         var totalSize = size+1;
@@ -68,8 +214,8 @@ const Arena = struct {
         }
         const result = self.heap;
         const hash = if (self.arenaType==.Test) 0 else @intCast(u24,@ptrToInt(result)%16777213);
-        const header = heap.header(@intCast(u16,size),form,classIndex,hash);
-        result.* = @bitCast(Object,header);
+        const head = header(@intCast(u16,size),form,classIndex,hash);
+        result.* = @bitCast(Object,head);
         const instVars = result.instVars();
         for (instVars) |_,index| {
             instVars[index]=fill;
@@ -81,13 +227,13 @@ const Arena = struct {
                 indexables[index]=fill;
             }
         }
-        return @ptrCast(heap.HeapPtr,result);
+        return @ptrCast(HeapPtr,result);
     }
-    pub inline fn allocObject(self : *Self, classIndex : class.ClassIndex, iv_size : usize) !heap.HeapPtr {
-        return self.alloc(classIndex, heap.Format.none, iv_size, 0, object.Nil);
+    pub inline fn allocObject(self : *Self, classIndex : class.ClassIndex, iv_size : usize) !HeapPtr {
+        return self.alloc(classIndex, Format.none, iv_size, 0, object.Nil);
     }
-    pub inline fn allocRaw(self : *Self, classIndex : class.ClassIndex, array_size : usize, fill: anytype) !heap.HeapPtr {
-        return self.alloc(classIndex, heap.Format.none, 0, array_size, fill);
+    pub inline fn allocRaw(self : *Self, classIndex : class.ClassIndex, array_size : usize, fill: anytype) !HeapPtr {
+        return self.alloc(classIndex, Format.none, 0, array_size, fill);
     }
 };
 const TestArena = struct {
@@ -111,13 +257,13 @@ const TestArena = struct {
         self.* = undefined;
         //testing.allocator.deinit();
     }
-    inline fn allocObject(self : *Self, classIndex : class.ClassIndex, iv_size : usize) !heap.HeapPtr {
+    inline fn allocObject(self : *Self, classIndex : class.ClassIndex, iv_size : usize) !HeapPtr {
         return self.heap.allocObject(classIndex, iv_size);
     }
-    inline fn allocRaw(self : *Self, classIndex : class.ClassIndex, array_size : usize, fill: anytype) !heap.HeapPtr {
+    inline fn allocRaw(self : *Self, classIndex : class.ClassIndex, array_size : usize, fill: anytype) !HeapPtr {
         return self.heap.allocRaw(classIndex, array_size, fill);
     }
-    inline fn alloc(self : *Self, classIndex : class.ClassIndex, format: heap.Format, iv_size : usize, array_size : usize, fill: anytype) !heap.HeapPtr {
+    inline fn alloc(self : *Self, classIndex : class.ClassIndex, format: Format, iv_size : usize, array_size : usize, fill: anytype) !HeapPtr {
         return self.heap.alloc(classIndex,format,iv_size,array_size,fill);
     }
     fn isEqual(self: Self) bool {
@@ -131,7 +277,7 @@ const TestArena = struct {
     }
 };
 test "simple object allocator" {
-    const h = heap.header(3,heap.Format.object,42,0);
+    const h = header(3,Format.object,42,0);
     const expected = [_]Object{h.asObject(),object.True,object.Nil,object.False};
     var testArena = try TestArena.init(expected[0..]);
     defer testArena.deinit();
