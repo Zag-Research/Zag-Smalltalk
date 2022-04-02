540a0dd0e31233f21f35877144dc2e2518d0e038
Dave Mason <dmason@ryerson.ca>
2022-04-01 20:57:40 -0400

    track unbounded stacks

--- zig/thread.zig
+++ zig/thread.zig
@@ -4,6 +4,7 @@ const Object = @import("object.zig").Object;
 const heap = @import("heap.zig");
 pub const Thread = struct {
     id : u64,
+    stackDepth: i32;
     heap : heap.Arena,
     const Self = @This();
     pub fn init() !Self {
@@ -30,6 +31,12 @@ pub const Thread = struct {
         if (self.heap.space()<1) unreachable;
         self.heap.tos -= 1;
         self.heap.tos[0]=obj;
+        self.stackDepth += 1;
+    }
+    pub inline fn pop(self: *Self, n: u8) void {
+        self.stackDepth -= n;
+        if (self.stackDepth<0) unreachable;
+        self.heap.tos += n;
     }
     pub inline fn getArena(self: *Self) *heap.Arena {
         return &self.heap;
