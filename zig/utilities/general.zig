7c478b6ab193c2feeb9187fa08fa1156537b14a1
Dave Mason <dmason@ryerson.ca>
2022-09-23 00:30:48 -0400

    Generalizing

--- zig/utilities/general.zig
+++ zig/utilities/general.zig
@@ -1,8 +1,19 @@
+const std = @import("std");
+const phi = std.math.phi;
 pub fn inversePhi(comptime T: type) T {
-    return switch (T) { // these are all 2^size/phi
-        u32 => 2654435769, // was 2717763491
-        u16 => 40503,
-        u8 => 158,
-        else => 11400714819323197440,
-    };
+    switch (@typeInfo(T)) {
+        .Int => |int_info| switch (int_info.signedness) {
+            .unsigned => return @floatToInt(T,(1<<int_info.bits)/phi),
+            else => {},
+        },
+        else => {},
+    }
+    @compileError("invalid type for inversePhi: "++@typeName(T));
+}
+test "check inversePhi" {
+    const expectEqual = std.testing.expectEqual;
+    try expectEqual(inversePhi(u32),2654435769);
+    try expectEqual(inversePhi(u16),40503);
+    try expectEqual(inversePhi(u8),158);
+    try expectEqual(inversePhi(u64),11400714819323198485);
 }
