61b308f7a0e4276b4d3f002a2b6b9f9ae03e8dfe
Dave Mason <dmason@ryerson.ca>
2022-09-23 23:59:32 -0400

    Moved string code to heap.zig

--- zig/heap.zig
+++ zig/heap.zig
@@ -722,3 +722,42 @@ fn alloc(h: Header) *Header {
         // use bigAllocation
     }
 }
+fn hash24(str: [] const u8) u24 {
+    const phi = @import("utilities.zig").inversePhi(u32);
+    var hash = phi*%(str.len+%1);
+    for (str) |c,idx| {
+        if (idx>9) return @truncate(u24,hash);
+        hash +%= phi*%c;
+    }
+    return @truncate(u24,hash);
+}
+pub fn CompileTimeString(comptime str: [] const u8) type {
+    const size = str.len;
+    const hash = hash24(str);
+    return struct {
+        header: Header,
+        chars: [size]u8,
+        const Self = @This();
+        pub fn init() Self {
+            var result : Self = .{
+                .header = header((size+7)/8,Format.immutable.raw(u8,size),class.String_I,hash,15),
+                .chars = [_]u8{0}**size,
+            };
+            for (str) |c,idx| {
+                result.chars[idx]=c;
+            }
+            return result;
+        }
+        fn h(self: * const Self) []const u8 {
+            return @ptrCast([*]const u8,self)[0..(size+15)/8*8];
+        }
+        pub fn o(self: * const Self) Object {
+            return Object.from(@ptrCast(*const Header,self));
+        }
+    };
+}
+const abcde = CompileTimeString("abcde").init();
+test "compile time" {
+    std.debug.print("abcde: {any}\n",.{abcde.h()});
+    std.debug.print("abcde: {any}\n",.{abcde.o()});
+}
