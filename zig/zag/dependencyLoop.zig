1982b363debff8ebcf939cc3916e2eab22bd433d
Dave Mason <dmason@ryerson.ca>
2025-01-19 12:39:07 -0500

    The depndency loop exemplar is now minimal
    https://github.com/ziglang/zig/issues/22543

--- zig/zag/dependencyLoop.zig
+++ zig/zag/dependencyLoop.zig
@@ -2,51 +2,19 @@ const std = @import("std");
 
 const execute = struct {
     const ThreadedFn = *const fn (
-//        programCounter: PC, stackPointer: SP,
         process: *Process,
         context: *Context,
-        signature: MethodSignature
-    ) SP;
-    const CompiledMethodPtr = *CompiledMethod;
-    const CompiledMethod = struct {
-//        executeFn: ThreadedFn,
-//        jitted: ThreadedFn,
-    };
-    const PC = struct {
-        code: *const Code,
-    };
-    const Code = union {
-//        prim: ThreadedFn,
-        int: i64,
-    };
-    const Stack = extern struct {
-        top: u64,
-    };
-    const SP = *Stack;
-    const MethodSignature = extern struct {
-        int: u64,
-    };
+    ) void;
 };
 const Process = extern struct {
-    int: u64,
-    debugFn: ?ThreadedFn,
-    sp: SP,
-    const ThreadedFn = execute.ThreadedFn;
-    const SP = execute.SP;
+    debugFn: ?execute.ThreadedFn,
 };
 const Context = struct {
-    method: CompiledMethodPtr,
-    npc: ThreadedFn,
-    prevCtxt: ?ContextPtr,
-    const ThreadedFn = execute.ThreadedFn;
-    const CompiledMethodPtr = execute.CompiledMethodPtr;
-    const ContextPtr = *Context;
+    npc: execute.ThreadedFn,
 };
 
 test "die" {
     _ = Process{
-        .int=42,
         .debugFn=null,
-        .sp=undefined,
     };
 }
