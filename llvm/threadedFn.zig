3540389b7549f5315b9ca65b5fdf03e1d604ca3b
JanBaig <janatbaig2@gmail.com>
2025-03-03 13:09:48 -0500

    [JIT] Decouple LLVM function signature setup from body emission

--- llvm/threadedFn.zig
+++ llvm/threadedFn.zig
@@ -25,10 +25,29 @@ pub fn main() !void {
     const builder: types.LLVMBuilderRef = core.LLVMCreateBuilder();
 
     // Create a ThreadedFn in IR
-    _ = try populateModule(module, builder);
+    const threadedFnSig = try createThreadedFnSig(module, "pushLiteral");
+    _ = try createPushLiteralFn(threadedFnSig, builder);
+    _ = try verifyModule(module);
+
+    // Cleanup builder
+    core.LLVMDisposeBuilder(builder);
+
+    printModule(module);
 }
 
-pub fn populateModule(module: types.LLVMModuleRef, builder: types.LLVMBuilderRef) !types.LLVMModuleRef {
+pub fn createPushLiteralFn(threadedFn: types.LLVMValueRef, builder: types.LLVMBuilderRef) !void {
+
+    // Setup function body
+    const entryBB: types.LLVMBasicBlockRef = core.LLVMAppendBasicBlock(threadedFn, "entry");
+    core.LLVMPositionBuilderAtEnd(builder, entryBB);
+
+    // To be implemented.
+
+    const spParam = core.LLVMGetParam(threadedFn, 1);
+    _ = core.LLVMBuildRet(builder, spParam);
+}
+
+pub fn createThreadedFnSig(module: types.LLVMModuleRef, fnName: [:0]const u8) !?*types.LLVMOpaqueValue {
     const ctx = core.LLVMGetGlobalContext();
     const typeInt64 = core.LLVMInt64TypeInContext(ctx);
 
@@ -76,7 +95,7 @@ pub fn populateModule(module: types.LLVMModuleRef, builder: types.LLVMBuilderRef
     var paramTypes: [5]types.LLVMTypeRef = .{ pcTy, stackPtrTy, processPtrTy, contextPtrTy, typeInt64 }; // missing context
     const threadedFnTy = core.LLVMFunctionType(stackPtrTy, &paramTypes[0], 5, 0);
     const threadedFnPtrTy = core.LLVMPointerType(threadedFnTy, 0);
-    const threadedFn: types.LLVMValueRef = core.LLVMAddFunction(module, "pushLiteral", threadedFnTy);
+    const threadedFn: types.LLVMValueRef = core.LLVMAddFunction(module, fnName, threadedFnTy);
 
     // Add the `CompiledMethod` fields
     var compiledMethodFields = [_]types.LLVMTypeRef{
@@ -89,7 +108,7 @@ pub fn populateModule(module: types.LLVMModuleRef, builder: types.LLVMBuilderRef
     };
     core.LLVMStructSetBody(compiledMethodTy, &compiledMethodFields[0], 6, 0);
 
-    // Add the `Context fields`
+    // Add the `Context` fields
     var contextFields = [_]types.LLVMTypeRef{
         typeInt64, // heapHeader
         compiledMethodPtrTy, // method
@@ -115,17 +134,17 @@ pub fn populateModule(module: types.LLVMModuleRef, builder: types.LLVMBuilderRef
     core.LLVMSetValueName(ctxParam, "context");
     core.LLVMSetValueName(sigParam, "signature");
 
-    // Setup function body
-    const entryBB: types.LLVMBasicBlockRef = core.LLVMAppendBasicBlock(threadedFn, "entry");
-    core.LLVMPositionBuilderAtEnd(builder, entryBB);
+    // // Only for testing purposes - enables emission of types textually in the IR
+    // _ = core.LLVMBuildAlloca(builder, stackTy, "test-stack");
+    // _ = core.LLVMBuildAlloca(builder, tagObjectTy, "test-tagObject");
+    // _ = core.LLVMBuildAlloca(builder, codeUnionTy, "test-union");
+    // _ = core.LLVMBuildAlloca(builder, contextTy, "test-context");
+    // _ = core.LLVMBuildRet(builder, spParam); // only to adhere to the return type
 
-    // Only for testing purposes - enables emission of types textually in the IR
-    _ = core.LLVMBuildAlloca(builder, stackTy, "test-stack");
-    _ = core.LLVMBuildAlloca(builder, tagObjectTy, "test-tagObject");
-    _ = core.LLVMBuildAlloca(builder, codeUnionTy, "test-union");
-    _ = core.LLVMBuildAlloca(builder, contextTy, "test-context");
-    _ = core.LLVMBuildRet(builder, spParam); // only to adhere to the return type
+    return threadedFn;
+}
 
+pub fn verifyModule(module: types.LLVMModuleRef) !void {
     // Verify the module and capture the message
     var errorMessage: ?[*:0]u8 = null;
     if (analysis.LLVMVerifyModule(module, types.LLVMVerifierFailureAction.LLVMPrintMessageAction, &errorMessage) != 0) {
@@ -139,15 +158,12 @@ pub fn populateModule(module: types.LLVMModuleRef, builder: types.LLVMBuilderRef
     } else {
         std.debug.print("Module verification passed.\n", .{});
     }
+}
 
+pub fn printModule(module: types.LLVMModuleRef) void {
     std.debug.print("\n--- IR DUMP ---\n", .{});
     core.LLVMDumpModule(module);
     std.debug.print("--- END IR ---\n\n", .{});
-
-    // Cleanup builder
-    core.LLVMDisposeBuilder(builder);
-
-    return module;
 }
 
 inline fn buildGEP(builder: types.LLVMBuilderRef, elementType: types.LLVMTypeRef, base: types.LLVMValueRef, offset: i64, name: []const u8) types.LLVMValueRef {
