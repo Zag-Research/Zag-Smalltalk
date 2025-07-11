fd4c2b7e5b535a690e633f5cf4c15b1d429cfefd
Dave Mason <dmason@ryerson.ca>
2025-07-11 09:48:55 -0400

    x86_64 data added

--- zig/experiments/branchPrediction.zig
+++ zig/experiments/branchPrediction.zig
@@ -9,22 +9,22 @@ const Tests = enum {
     T8F8,
     T16F16,
 };
-const test_type: Tests = .T2F2;
+const test_type: Tests = .TF;
 pub fn main() u8 {
     const max: usize = 10_000_000_000;
     var sum: usize = 0;
     for (0..max) |i| {
         // Let’s measure the performance of this loop with different conditions:
-        if (switch (test_type) {           // M2Pro
-            .T_repeated => i & 0x80000000, // 1.84s
-            .F_repeated => i & 0xffffffff, // 1.84s
-            .TF => i & 1,                  // 1.55s
-            .T2F2 => i & 2,                // 1.68s
-            .TFFF => i & 3,                // 1.82s
-            .T4F4 => i & 4,                // 1.82s
-            .TFTFFFFF => i & 5,            // 1.84s
-            .T8F8 => i & 8,                // 1.84s
-            .T16F16 => i & 16,             // 1.84s
+        if (switch (test_type) {           // M2Pro   i7-2.6GHz
+            .T_repeated => i & 0x80000000, // 1.84s    1.15s
+            .F_repeated => i & 0xffffffff, // 1.84s    1.15s
+            .TF => i & 1,                  // 1.55s    0.75s
+            .T2F2 => i & 2,                // 1.68s    0.76s
+            .TFFF => i & 3,                // 1.82s    0.75s
+            .T4F4 => i & 4,                // 1.82s    0.87s
+            .TFTFFFFF => i & 5,            // 1.84s    1.05s
+            .T8F8 => i & 8,                // 1.84s    1.10s
+            .T16F16 => i & 16,             // 1.84s    1.10s
             } == 0) sum += i;
     }
     return @truncate(sum);
