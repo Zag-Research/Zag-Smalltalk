346a5f5b85a39ed33fe14ffa3ea20d22038aa42b
Dave Mason <dmason@ryerson.ca>
2026-01-24 18:00:48 -0500

    JIT code extended (not tested) to x86_64, windows, and linux

--- zig/experiments/returnAddress.zig
+++ zig/experiments/returnAddress.zig
@@ -2,45 +2,171 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-// 1. External C declarations for macOS specific functions
-// We link libc (-lc) to access these.
-extern "c" fn pthread_jit_write_protect_np(enabled: c_int) void;
-extern "c" fn sys_icache_invalidate(start: ?*const anyopaque, size: usize) void;
-const aarch64 = struct {
-    fn enable_write_permission() void {
-        // 5. Enable Write Permissions (Apple Silicon only)
-        // 0 = disable protection (allow write)
-        if (builtin.cpu.arch == .aarch64) {
-            pthread_jit_write_protect_np(0);
+const common = struct {
+    extern "c" fn pthread_jit_write_protect_np(enabled: c_int) void;
+    fn mmap(size: usize) ![]align(std.heap.page_size_min) u8 {
+        switch (builtin.os.tag) {
+            .windows => {
+                const MEM_COMMIT = std.os.windows.MEM_COMMIT;
+                const MEM_RESERVE = std.os.windows.MEM_RESERVE;
+                const PAGE_READWRITE = std.os.windows.PAGE_READWRITE;
+                const ptr = std.os.windows.VirtualAlloc(
+                    null,
+                    size,
+                    MEM_COMMIT | MEM_RESERVE,
+                    PAGE_READWRITE,
+                ) catch return error.OutOfMemory;
+                return @as([*]u8, @ptrCast(ptr))[0..size];
+            },
+            .macos, .linux => {
+                const prot_rwx = std.posix.PROT.READ | std.posix.PROT.WRITE | std.posix.PROT.EXEC;
+                return std.posix.mmap(null, size, prot_rwx, .{ .TYPE = .PRIVATE, .ANONYMOUS = true, .JIT = true }, -1, 0);
+            },
+            else => unreachable,
         }
     }
-    fn disable_write_permission() void {
-        // 5. Enable Write Permissions (Apple Silicon only)
-        // 0 = disable protection (allow write)
-        if (builtin.cpu.arch == .aarch64) {
-            pthread_jit_write_protect_np(1);
+
+    fn munmap(mem: []align(std.heap.page_size_min) u8) void {
+        switch (builtin.os.tag) {
+            .windows => _ = std.os.windows.VirtualFree(mem.ptr, 0, std.os.windows.MEM_RELEASE),
+            .macos, .linux => std.posix.munmap(mem),
+            else => unreachable,
+        }
+    }
+
+    fn enable_write_permission(mem: []u8) void {
+        switch (builtin.os.tag) {
+            .macos => pthread_jit_write_protect_np(0),
+            .windows => {
+                const PAGE_READ_WRITE = std.os.windows.PAGE_READ_WRITE;
+                var old_prot: u32 = undefined;
+
+                // 1. Change Permissions
+                // Note: Zig's std.os.windows.VirtualProtect wrapper might be minimal,
+                // so we call the kernel32 function directly if needed, but standard library usage is:
+                const res = std.os.windows.VirtualProtect(
+                    mem.ptr,
+                    mem.len,
+                    PAGE_READ_WRITE,
+                    &old_prot,
+                );
+                if (res == 0) return error.AccessDenied;
+            },
+            .linux => {
+                // We must remove execute permission to satisfy W^X
+                const prot_rw = std.posix.PROT.READ | std.posix.PROT.WRITE;
+                return std.posix.mprotect(mem.ptr, prot_rw);
+            },
+            else => unreachable,
+        }
+    }
+};
+
+const aarch64 = struct {
+    const mmap = common.mmap;
+    const munmap = common.munmap;
+    extern "c" fn pthread_jit_write_protect_np(enabled: c_int) void;
+    extern "c" fn sys_icache_invalidate(start: ?*const anyopaque, size: usize) void;
+    pub const enable_write_permission = common.enable_write_permission;
+    extern "c" fn __clear_cache(start: [*]u8, end: [*]u8) void;
+    pub fn prepare_to_execute(mem: []u8) void {
+        switch (builtin.os.tag) {
+            .macos => {
+                pthread_jit_write_protect_np(1);
+                sys_icache_invalidate(mem.ptr, mem.len);
+            },
+            .windows => {
+                var old_protection: std.os.windows.PROTECTION_TYPE = undefined;
+                _ = std.os.windows.VirtualProtect(mem.ptr, mem.len, std.os.windows.PAGE_EXECUTE_READ, &old_protection);
+                const process = std.os.windows.GetCurrentProcess();
+                _ = std.os.windows.kernel32.FlushInstructionCache(
+                    process,
+                    mem.ptr,
+                    mem.len,
+                );
+            },
+            .linux => {
+                try std.posix.mprotect(mem, std.posix.PROT.READ | std.posix.PROT.EXEC);
+                const clear_cache = @extern(fn ([*]u8, [*]u8) void, .{ .name = "__clear_cache" });
+                clear_cache(mem.ptr, mem.ptr + mem.len);
+            },
+            else => unreachable,
         }
     }
 
     const call_prefix = [_]u8{ 0x50, 0x00, 0x00, 0x58, 0x00, 0x02, 0x3F, 0xD6 };
     pub const threaded_code_offset = 8;
     const header_length = call_prefix.len + @sizeOf(*const fn_type);
-    fn write_call_code(mem: [*]align(@alignOf(fn_type)) u8, ptr: *const fn_type) *fn_type {
+    pub fn write_call_code(mem: [*]align(@alignOf(fn_type)) u8, ptr: *const fn_type) *fn_type {
         @memcpy(mem[0..call_prefix.len], &call_prefix);
         const addr: **const fn_type = @ptrFromInt(@intFromPtr(mem) + call_prefix.len);
         addr.* = ptr;
         return @ptrCast(mem);
     }
+};
+const x86_64 = struct {
+    const mmap = common.mmap;
+    const munmap = common.munmap;
+    pub const enable_write_permission = common.enable_write_permission;
+    extern "c" fn pthread_jit_write_protect_np(enabled: c_int) void;
+    pub fn prepare_to_execute(mem: []u8) void {
+        switch (builtin.os.tag) {
+            .macos => pthread_jit_write_protect_np(1),
+            .windows => {
+                const PAGE_EXECUTE_READ = std.os.windows.PAGE_EXECUTE_READ;
+                var old_prot: u32 = undefined;
 
-    fn invalidate_instruction_cache(mem: []u8) void {
-        sys_icache_invalidate(mem.ptr, mem.len);
+                // 1. Change Permissions
+                // Note: Zig's std.os.windows.VirtualProtect wrapper might be minimal,
+                // so we call the kernel32 function directly if needed, but standard library usage is:
+                const res = std.os.windows.VirtualProtect(
+                    mem.ptr,
+                    mem.len,
+                    PAGE_EXECUTE_READ,
+                    &old_prot,
+                );
+                if (res == 0) return error.AccessDenied;
+
+                // 2. Flush Cache (Required for AARCH64, good practice for x64)
+                // -1 as process handle means "current process"
+                _ = std.os.windows.kernel32.FlushInstructionCache(
+                    std.os.windows.GetCurrentProcess(),
+                    mem.ptr,
+                    mem.len,
+                );
+            },
+            .linux => {
+                // We must remove Write permission to satisfy W^X
+                const prot_rx = std.posix.PROT.READ | std.posix.PROT.EXEC;
+                return std.posix.mprotect(mem.ptr, prot_rx);
+            },
+            else => unreachable,
+        }
+    }
+
+    const call_prefix = [_]u8{ 0x48, 0xB8 };
+    const call_suffix = [_]u8{ 0xFF, 0xD0 };
+    pub const threaded_code_offset = 4;
+    const header_length = call_prefix.len + @sizeOf(*const fn_type) + call_suffix.len;
+    pub fn write_call_code(mem: [*]align(@alignOf(fn_type)) u8, ptr: *const fn_type) *fn_type {
+        @memcpy(mem[0..call_prefix.len], &call_prefix);
+        const addr: **const fn_type = @ptrFromInt(@intFromPtr(mem) + call_prefix.len);
+        addr.* = ptr;
+        const suffix_offset = call_prefix.len + @sizeOf(*const fn_type);
+        @memcpy(mem[suffix_offset .. suffix_offset + call_suffix.len], &call_suffix);
+        return @ptrCast(mem);
     }
 };
+
 pub const arch = switch (builtin.cpu.arch) {
     .aarch64 => aarch64,
+    .x86_64 => x86_64,
     else => unreachable,
 };
-pub const threaded_header_length = (@max(arch.threaded_code_offset, 0) + 7) & ~@as(usize, 7);
+pub const threaded_header_length = (@max(
+    aarch64.header_length,
+    x86_64.header_length,
+) + 7) & ~@as(usize, 7);
 const fn_type = fn (sp: [*]u64) void;
 fn expect(sp: [*]u64) void {
     const fpc: [*]u8 = @ptrFromInt(@returnAddress() + arch.threaded_code_offset);
@@ -53,23 +179,16 @@ fn expect(sp: [*]u64) void {
 }
 pub fn main() !void {
     // This returns a slice []align(page_size) u8
-    const mem = try std.posix.mmap(null, std.heap.page_size_min, std.posix.PROT.READ | std.posix.PROT.WRITE | std.posix.PROT.EXEC, .{ .TYPE = .PRIVATE, .ANONYMOUS = true, .JIT = true }, -1, 0);
-    // Ensure we unmap when done
-    defer std.posix.munmap(mem);
+    const mem = try arch.mmap(std.heap.page_size_min);
+    defer arch.munmap(mem);
 
-    arch.enable_write_permission();
+    arch.enable_write_permission(mem);
     for (mem, 0..) |*m, i|
         m.* = @truncate(i);
-    //const funcCall: *fn([*]u64) void = @ptrCast(mem.ptr);
-    // 6. Copy code into memory
     const size = threaded_header_length;
-    // x86_64: mov rax, 42; ret
-    //const code_x86_prefix = [_]u8{ 0x48, 0xB8};
-    //const code_x86_suffic = [_]u8{ 0xFF, 0xD0};
 
     const funcCall = arch.write_call_code(mem.ptr, &expect);
-    arch.disable_write_permission();
-    arch.invalidate_instruction_cache(mem);
+    arch.prepare_to_execute(mem);
 
     var stack = [_]u64{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     std.debug.print("expect: {*}\n", .{&expect});
