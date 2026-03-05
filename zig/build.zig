a82903ac455db7b35c5fc2d276631578e8b78d15
Nishil <nishilkapadia17@gmail.com>
2026-03-05 00:07:22 -0500

    use llvm back

--- zig/build.zig
+++ zig/build.zig
@@ -350,7 +350,7 @@ fn createBenchStep(
                 },
                 .omit_frame_pointer = build_options.omit_frame_pointer,
             }),
-            .use_llvm = false,
+            .use_llvm = true,
         });
 
         const run_bench = b.addRunArtifact(bench_exe);
