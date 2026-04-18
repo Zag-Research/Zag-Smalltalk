fcac6faaa6427404f8861d35ddc693f56dd7df02
Nishil <nishilkapadia17@gmail.com>
2026-04-17 23:23:00 -0400

    remove tf-asm

--- zig/experiments/cnp/tf-asm/primitives.Array.threadedFns.array.threadedFn.s
+++
@@ -1,7 +0,0 @@
-; symbol: _primitives.Array.threadedFns.array.threadedFn
-; object: /Users/nish7/Code/not-mine/Zag-Smalltalk/zig/zig-out/lib/cnp-fib-bench.o
-
-_primitives.Array.threadedFns.array.threadedFn:
-    7550:	fd 7b bf a9	stp	x29, x30, [sp, #-0x10]!
-    7554:	fd 03 00 91	mov	x29, sp
-    7558:	20 00 20 d4	brk	#0x1
